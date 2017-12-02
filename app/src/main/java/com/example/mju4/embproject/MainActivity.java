54dd274929841d71bb34a4379710126c8906180f
gitdog01 <hostdog01@naver.com>
2017-12-02 13:47:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mju4/embproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mju4.embproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pressbutton(View view) {
+    }
+}
